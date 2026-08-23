539eb074df086ca6b8acd08b6cf4abaa9751967b
meronemo <seanchoi227@gmail.com>
2026-08-23 17:13:23 +0900

    chore: set Gmail sender display name to HAFS PrintQueue

--- app/email_service.py
+++ app/email_service.py
@@ -16,8 +16,9 @@ SMTP_PASS = os.getenv("SMTP_PASS")
 def send_email(to: str, subject: str, html: str):
     context = ssl.create_default_context()
     msg = EmailMessage()
+    from_str = f"HAFS PrintQueue <${SMTP_USER}>"
     msg["Subject"] = subject
-    msg["From"] = SMTP_USER
+    msg["From"] = from_str
     msg["To"] = to
     msg.set_content(html, subtype="html")
 
